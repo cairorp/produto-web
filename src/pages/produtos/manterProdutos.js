fd9eddee52c87ca26b74cdc35ff9c897b41802c7
Cairo Pereira <cairo.pereira@mbra.com.br>
2025-05-25 19:10:25 -0300

    Remover imports não utilizados

--- src/pages/produtos/manterProdutos.js
+++ src/pages/produtos/manterProdutos.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { InputNumber } from 'primereact/inputnumber';
 import { Button } from 'primereact/button';
